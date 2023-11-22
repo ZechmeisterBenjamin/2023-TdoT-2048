0a15e99f6a51bdb48a21dfcc0b45142e07fe0b6b
Zechmeister Benjamin <benjamin.zechmeister@htlwy.at>
2023-11-22 21:10:02 +0100

    Assigned Buttons to slots array

--- TdoT-2048-WPF/MainWindow.xaml.cs
+++ TdoT-2048-WPF/MainWindow.xaml.cs
@@ -22,10 +22,11 @@ namespace TdoT_2048_WPF
     {
         static bool addNmb = false;
         static int score = 0;
-        string[,] slots = new string[4, 4];
+        Button[,] slots = new Button[4, 4];
         public MainWindow()
         {
             InitializeComponent();
+            FillSlots();
             Main_Window.KeyDown += new KeyEventHandler(OnButtonKeyDown);
         }
         private void OnButtonKeyDown(object sender, KeyEventArgs e)
@@ -36,6 +37,26 @@ namespace TdoT_2048_WPF
         private void MoveCube(Key direction)
         {
             throw new NotImplementedException();
+        }
+        private void FillSlots()
+        {
+            slots[0, 0] = TL;
+            slots[1, 0] = TML;
+            slots[2, 0] = TMR;
+            slots[3, 0] = TR;
+            slots[0, 1] = TML;
+            slots[1, 1] = TMLM;
+            slots[2, 1] = TMRM;
+            slots[3, 1] = TMR;
+            slots[0, 2] = BML;
+            slots[1, 2] = BMLM;
+            slots[2, 2] = BMRM;
+            slots[3, 2] = BMR;
+            slots[0, 3] = BL;
+            slots[1, 3] = BLM;
+            slots[2, 3] = BRM;
+            slots[3, 3] = BR;
+
         }
     }
 }
